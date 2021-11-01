3f8aa7551b6bfec74aafbf0dff5f4656e6626af2
Alexey Natalin <mrreggies@gmail.com>
2021-11-01 18:19:19 +0700

    edk2-test: refurbished the audio protocol interface

--- edk2-test/RustTestPkg/Application/Launcher/Launcher.c
+++ edk2-test/RustTestPkg/Application/Launcher/Launcher.c
@@ -29,29 +29,82 @@ CONST STATIC struct {
   { 8000, 100 },
 };
 
-#if 0
+INT16 Lerpi(INT16 X0, INT16 X1, UINTN Numer, UINTN Denom)
+{
+  return (INT16)((INTN) X0 * ((INTN) Denom - (INTN) Numer) / (INTN) Denom +
+      (INT16)((INTN) X1 * (INTN) Numer / (INTN) Denom));
+  return 0;
+}
+
 EFI_STATUS
-PlayTest(EFI_SIMPLE_AUDIO_OUT_PROTOCOL *SimpleAudioOut)
+PcmTest(EFI_SIMPLE_AUDIO_OUT_PROTOCOL *SimpleAudioOut)
 {
   EFI_STATUS Status;
   UINTN Index;
+  INT16 *Samples;
+  UINTN SampleCount;
+  CONST UINT32 kFrequencies[] = {
+    2*260,
+    2*480,
+    2*170,
+    4*260,
+    4*480,
+    4*170,
+    1*260,
+    1*480,
+    1*170,
+  };
+  UINT8 ChannelCount = 2;
+  UINT32 SamplingRate = EFI_AUDIO_RATE_22050;
+  UINTN Period;
+  UINTN WritePointer;
+  UINTN ChannelIndex;
 
-  DEBUG((EFI_D_VERBOSE, "PlayTest Protocol: %p\n", SimpleAudioOut));
+  DEBUG((EFI_D_VERBOSE, "PcmTest Protocol: %p\n", SimpleAudioOut));
 
-  for (Index = 0; Index < ARRAY_SIZE(kTestTone); ++Index)
+  SampleCount = ChannelCount * SamplingRate / 3;
+  Samples = AllocateZeroPool(SampleCount * sizeof(INT16));
+  if (Samples == NULL) {
+    return RETURN_OUT_OF_RESOURCES;
+  }
+
+  for (Index = 0; Index < ARRAY_SIZE(kFrequencies); ++Index)
   {
-    Status = SimpleAudioOut->Feed(SimpleAudioOut, 14000, 50);
+    /* Generate some mess */
+    for(WritePointer = 0; WritePointer * ChannelCount < SampleCount; ++WritePointer) {
+      for(ChannelIndex = 0; ChannelIndex < ChannelCount; ++ChannelIndex) {
+        Period = (SamplingRate + kFrequencies[Index] - 1) / kFrequencies[Index];
+        Samples[ChannelCount*WritePointer+ChannelIndex] = Lerpi(
+          MIN_INT16,
+          0,
+          WritePointer % Period,
+          Period);
+      }
+    }
+
+    /* Play some mess */
+    Status = SimpleAudioOut->Write(
+        SimpleAudioOut,
+        SamplingRate,
+        ChannelCount,
+        EFI_AUDIO_FORMAT_S16LE,
+        Samples,
+        SampleCount);
     if (EFI_ERROR(Status)) {
-      DEBUG((EFI_D_ERROR, "Feed (%d) returned %p\n", Index, Status));
-      return Status;
+      DEBUG((EFI_D_ERROR, "Write (%d) returned %p\n", Index, Status));
+      goto Cleanup;
     }
   }
 
-  DEBUG((EFI_D_VERBOSE, "ToneTest done\n"));
+  DEBUG((EFI_D_VERBOSE, "PcmTest done\n"));
+  Status = RETURN_SUCCESS;
 
-  return EFI_SUCCESS;
+Cleanup:
+  if (Samples) {
+    FreePool(Samples);
+  }
+  return Status;
 }
-#endif
 
 EFI_STATUS
 ToneTest(EFI_SIMPLE_AUDIO_OUT_PROTOCOL *SimpleAudioOut)
@@ -63,7 +116,7 @@ ToneTest(EFI_SIMPLE_AUDIO_OUT_PROTOCOL *SimpleAudioOut)
 
   for (Index = 0; Index < ARRAY_SIZE(kTestTone); ++Index)
   {
-    Status = SimpleAudioOut->Tone(SimpleAudioOut, 14000, 50);
+    Status = SimpleAudioOut->Tone(SimpleAudioOut, 260, 1000);
     if (EFI_ERROR(Status)) {
       DEBUG((EFI_D_ERROR, "Tone (%d) returned %p\n", Index, Status));
       return Status;
@@ -100,19 +153,6 @@ UefiMain(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *SystemTable)
 
   DEBUG((EFI_D_VERBOSE, "Got %d handles\n", HandleCount));
 
-  // for(Index = 0; Index < HandleCount; ++Index) {
-  //   EFI_SIMPLE_AUDIO_OUT_PROTOCOL *SimpleAudioOut;
-  //   Status = gBS->HandleProtocol (
-  //       HandleBuffer[Index],
-  //       &gEfiSimpleAudioOutProtocolGuid,
-  //       (VOID **) &SimpleAudioOut);
-  //   if (EFI_ERROR(Status)) {
-  //     DEBUG((EFI_D_WARN, "HandleProtocol returned %r\n", Status));
-  //     continue;
-  //   }
-  //   PlayTest(SimpleAudioOut);
-  // }
-
   for(Index = 0; Index < HandleCount; ++Index) {
     EFI_SIMPLE_AUDIO_OUT_PROTOCOL *SimpleAudioOut;
     Status = gBS->HandleProtocol (
@@ -123,6 +163,9 @@ UefiMain(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *SystemTable)
       DEBUG((EFI_D_WARN, "HandleProtocol returned %r\n", Status));
       continue;
     }
+    DEBUG((EFI_D_INFO, "Testing PCM samples\n"));
+    PcmTest(SimpleAudioOut);
+    DEBUG((EFI_D_INFO, "Testing Beep generator\n"));
     ToneTest(SimpleAudioOut);
   }
 
--- edk2-test/RustTestPkg/Include/Protocol/SimpleAudioOut.h
+++ edk2-test/RustTestPkg/Include/Protocol/SimpleAudioOut.h
@@ -8,32 +8,78 @@
 
 typedef struct _EFI_SIMPLE_AUDIO_OUT_PROTOCOL EFI_SIMPLE_AUDIO_OUT_PROTOCOL;
 
+typedef struct _EFI_SIMPLE_AUDIO_OUT_MODE EFI_SIMPLE_AUDIO_OUT_MODE;
+
+//
+// Device Capabilities
+//
+#define EFI_AUDIO_CAP_RESET         (0x1)
+#define EFI_AUDIO_CAP_WRITE         (0x2)
+#define EFI_AUDIO_CAP_TONE          (0x4)
+#define EFI_AUDIO_CAP_MODE          (0x8)
+
+//
+// Sampling Rate
+//
+#define EFI_AUDIO_RATE_8000         (8000)
+#define EFI_AUDIO_RATE_11025        (11025)
+#define EFI_AUDIO_RATE_16000        (16000)
+#define EFI_AUDIO_RATE_22050        (22050)
+#define EFI_AUDIO_RATE_32000        (32000)
+#define EFI_AUDIO_RATE_44100        (44100)
+#define EFI_AUDIO_RATE_48000        (48000)
+
+//
+// Sample Formats
+//
+#define EFI_AUDIO_FORMAT_S16LE      (0x0)
+
+typedef
+EFI_STATUS
+(EFIAPI * EFI_SIMPLE_AUDIO_OUT_QUERY_MODE) (
+  IN EFI_SIMPLE_AUDIO_OUT_PROTOCOL *This,
+  IN UINTN Index,
+  OUT EFI_SIMPLE_AUDIO_OUT_MODE *Mode
+  );
+
 typedef
 EFI_STATUS
 (EFIAPI * EFI_SIMPLE_AUDIO_OUT_TONE) (
-    IN EFI_SIMPLE_AUDIO_OUT_PROTOCOL *This,
-    IN INT16 Frequency,
-    IN UINT16 Duration
-    );
+  IN EFI_SIMPLE_AUDIO_OUT_PROTOCOL *This,
+  IN INT16 Frequency,
+  IN UINT16 Duration
+  );
 
 typedef
 EFI_STATUS
 (EFIAPI * EFI_SIMPLE_AUDIO_OUT_RESET) (
-    IN EFI_SIMPLE_AUDIO_OUT_PROTOCOL *This
-    );
+  IN EFI_SIMPLE_AUDIO_OUT_PROTOCOL *This
+  );
 
 typedef
 EFI_STATUS
-(EFIAPI * EFI_SIMPLE_AUDIO_OUT_FEED) (
-    IN EFI_SIMPLE_AUDIO_OUT_PROTOCOL *This,
-    IN INT16 *Samples,
-    IN UINTN SampleCount
-    );
+(EFIAPI * EFI_SIMPLE_AUDIO_OUT_WRITE) (
+  IN EFI_SIMPLE_AUDIO_OUT_PROTOCOL *This,
+  IN UINT32 SamplingRate,
+  IN UINT8 ChannelCount,
+  IN UINT32 SampleFormat,
+  IN INT16 *Samples,
+  IN UINTN SampleCount
+  );
 
 struct _EFI_SIMPLE_AUDIO_OUT_PROTOCOL {
-    EFI_SIMPLE_AUDIO_OUT_RESET Reset;
-    EFI_SIMPLE_AUDIO_OUT_FEED Feed;
-    EFI_SIMPLE_AUDIO_OUT_TONE Tone;
+  EFI_SIMPLE_AUDIO_OUT_RESET Reset;
+  EFI_SIMPLE_AUDIO_OUT_WRITE Write;
+  EFI_SIMPLE_AUDIO_OUT_TONE Tone;
+  EFI_SIMPLE_AUDIO_OUT_QUERY_MODE QueryMode;
+  UINTN MaxMode;
+  UINT32 Capabilities;
+};
+
+struct _EFI_SIMPLE_AUDIO_OUT_MODE {
+  UINT32 SamplingRate;
+  UINT8 ChannelCount;
+  UINT32 SampleFormat;
 };
 
 extern EFI_GUID gEfiSimpleAudioOutProtocolGuid;
